53861daf9eb773c9f9a962773fa9c217253ce299
Aleksandr Tikunov <77337707+RubbishCode@users.noreply.github.com>
2022-01-12 03:41:42 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='aio_qiwi_transactions',
     version='0.0.1',
 
-    url='https://github.com/RubbishCode/aio_qiwi_transactionse',
+    url='https://github.com/RubbishCode/aio_qiwi_transactions',
     author='Rubbish Code',
     author_email='rubbishcode@gmail.com',
     py_modules=['requests-async'],
