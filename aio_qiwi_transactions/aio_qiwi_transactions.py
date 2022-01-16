5410f7b5ff549c6e2792d86bed33a6aec51c3581
Aleksandr Tikunov <77337707+RubbishCode@users.noreply.github.com>
2022-01-16 05:04:18 +0300

    Update aio_qiwi_transactions.py

--- aio_qiwi_transactions/aio_qiwi_transactions.py
+++ aio_qiwi_transactions/aio_qiwi_transactions.py
@@ -50,7 +50,7 @@ class AioQiwiTransactions:
         async with aiohttp.ClientSession(trust_env=True) as session:
             async with session.get(
                 f'https://edge.qiwi.com/payment-history/v2/persons/{self.phone}/payments', 
-                params={'rows': 50}, headers={'authorization': 'Bearer '.join(self.token)}, timeout=500) as req:
+                data={'rows': 50}, headers={'authorization': 'Bearer '.join(self.token)}, timeout=500) as req:
 
                 pays_user_key = [(req.json()['data'][x]['comment']) for x in range(len(req.json()['data']))]
                 sum_pays_user = [float((req.json()['data'][x]['sum']['amount'])) for x in range(len(req.json()['data']))]
